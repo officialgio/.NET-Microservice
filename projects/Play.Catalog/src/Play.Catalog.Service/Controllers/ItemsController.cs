02c29df69fb7ba2e77d38bfa962201799bc12b81
officialgio121 <officialgio121@gmail.com>
2024-02-02 11:46:14 -0600

    Added Starter Project

---
+++ projects/Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using static Play.Catalog.Service.Dtos;
+
+namespace Play.Catalog.Service.Controllers;
+
+[ApiController]
+[Route("items")] // https://localhost5001/items
+public class ItemsController : ControllerBase
+{
+	private static readonly List<ItemDto> items = new()
+	{
+		new ItemDto(Guid.NewGuid(), "Potion", "Restores all amount of HP", 5, DateTimeOffset.UtcNow),
+		new ItemDto(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
+		new ItemDto(Guid.NewGuid(), "Bronze Sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow)
+	};
+
+	[HttpGet]
+	public IEnumerable<ItemDto> Get()
+	{
+		return items;
+	}
+
+	[HttpGet("{id}")] // GET /items/{id}
+	public ItemDto? GetById(Guid id)
+	{
+        var item = items.Where(item => item.id == id).SingleOrDefault();
+		return item;
+	}
+
+	[HttpPost]
+	public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+	{
+		var item = new ItemDto(
+			Guid.NewGuid(),
+			createItemDto.Name,
+			createItemDto.Description,
+			createItemDto.Price,
+			DateTimeOffset.Now
+			);
+
+		items.Add(item);
+
+		return CreatedAtAction(nameof(GetById), new { id = item.id }, item);
+	}
+
+	[HttpPut]
+	public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+	{
+		var existingItem = items.Where(item => item.id == id).SingleOrDefault();
+		var updatedItem = existingItem with
+		{
+			Name = updateItemDto.Name,
+			Description = updateItemDto.Description,
+			Price = updateItemDto.Price,
+		};
+
+		var index = items.FindIndex(existingItem => existingItem.id == id);
+		items[index] = updatedItem;
+
+		return NoContent();
+	}
+
+	[HttpDelete("{id}")]
+	public IActionResult Delete(Guid id)
+	{
+		var index = items.FindIndex(existingItem => existingItem.id == id);
+		items.RemoveAt(index);
+		return NoContent();
+	}
+
+
+}
+
---
+++ projects/Play.Catalog/src/Play.Catalog.Service/Dtos.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Play.Catalog.Service;
+
+public class Dtos
+{
+	public record ItemDto(Guid id, string Name, string Description, decimal Price, DateTimeOffset createdDate);
+	public record CreateItemDto(string Name, string Description, decimal Price);
+	public record UpdateItemDto(string Name, string Description, decimal Price);
+}
+
