6249faa9aa2ecb70293ae113781eaf855604a2ce
officialgio121 <officialgio121@gmail.com>
2024-03-12 14:54:21 -0500

    Added Initial State Machine

---
+++ projects/Play.Trading/src/Play.Trading.Service/Contracts.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Play.Trading.Service;
+
+public record PurchaseRequested(
+    Guid UserId,
+    Guid ItemId,
+    int Quantity,
+    Guid CorrelationId);
---
+++ projects/Play.Trading/src/Play.Trading.Service/StateMachines/PurchaseStateMachine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Automatonymous;
+
+namespace Play.Trading.Service.StateMachines;
+
+/// <summary>
+/// This class is the actual concrete State Machine.
+/// </summary>
+public class PurchaseStateMachine : MassTransitStateMachine<PurchaseState>
+{
+	public State Accepted { get;  }
+	public State ItemsGranted { get; }
+	public State Completed { get; }
+	public State Faulted { get; }
+
+	public Event<PurchaseRequested> PurchaseRequested { get; }
+
+	public PurchaseStateMachine()
+	{
+		// Track the state
+		InstanceState(state => state.CurrentState);
+		ConfigureEvents();
+		ConfigureInitialState();
+    }
+
+	// MassTransit will make use of any necessary events in this method.
+	private void ConfigureEvents()
+	{
+		Event(() => PurchaseRequested);
+	}
+
+    private void ConfigureInitialState()
+	{
+        // When receiving a PurchaseRequested event, then we must obtain all of these properties.
+        Initially(
+			When(PurchaseRequested)
+				.Then(context =>
+				{
+                    // For clarification, instance = this class instance and Data = Incoming Request (i.e PurchaseRequested)
+                    context.Instance.UserId = context.Data.UserId;
+					context.Instance.ItemId = context.Data.ItemId;
+					context.Instance.Quantity = context.Data.Quantity;
+					context.Instance.Received = DateTimeOffset.Now;
+					context.Instance.LastUpdated = DateTimeOffset.Now;
+				})
+				.TransitionTo(Accepted)
+		);
+	}
+}
